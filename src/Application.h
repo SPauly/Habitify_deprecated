5f6ec5a4279087eb933040117466b9cedf17ac79
S I M O N. P <spau@hotmail.de>
2022-11-09 17:59:36 +0100

    initial succesfull build of ImGui Demo

---
+++ src/Application.h
@@ -0,0 +1,39 @@
+#pragma once
+// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
+// Read online: https://github.com/ocornut/imgui/tree/master/docs
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+
+namespace Habitify
+{
+    class Application
+    {
+    public:
+        Application() = default;
+        ~Application() = default;
+
+        void Run();
+        
+    private:
+        bool Init();
+        void Shutdown();
+    private:
+        // Demo dependencies
+        ImGuiIO io;
+        GLFWwindow *window;
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    };
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "Application.h"
+
+
+int main(){
+    Habitify::Application *app = new Habitify::Application();
+    app->Run();
+    return 0;
+}
