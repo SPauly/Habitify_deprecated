4bc857e5ca4ee67cde18ec92e6e6cbb96e735f99
S I M O N. P <spau@hotmail.de>
2023-03-07 23:18:32 +0100

    More generic Wrapper layout

--- utils/NodeWrapperUpdated.h
+++ utils/NodeWrapperUpdated.h
@@ -6,58 +6,54 @@
 
 namespace Habitify
 {
-    class NodeWrapper
+    class InterfaceBase
+    {
+    public:
+        virtual ~InterfaceBase()
+        {
+        }
+
+        virtual int32_t get_Id();
+        virtual void set_Id(HabCom::Node*);
+        virtual void sync_nodes();
+    };
+
+    template <typename T>
+    class NodeWrapper : public InterfaceBase
     {
     public:
         template <typename T>
-        NodeWrapper(T &&obj) : wrapped_node(std::make_shared<Wrapper<T>>(std::forward<T>(obj)))
+        NodeWrapper(T &&obj) : interface(std::make_shared<T>(std::forward<T>(obj)))
         {
         }
 
-    //this is the interface
-        class InternalBase
+        HabCom::Node *get_node() const
         {
-        public:
-            virtual ~InternalBase()
-            {
-            }
-
-            virtual int32_t get_Id();
-            virtual void sync_node();
-            virtual HabCom::Node* get_node();  
-        };
-
-        template <typename T>
-        class Wrapper : public InternalBase
-        {
-        public:
-            int32_t get_Id() override {
-                return wrapped_object.get_Id();
-            }
-            void sync_node() override
-            {
-                wrapped_object.sync_node();
-            }
-
-        private:
-            T wrapped_object;
-        };
-
-        int32_t get_Id(){
-            return wrapped_node->get_Id();
+            sync_nodes();
+            return interface->get_node();
         }
 
-        HabCom::Node* get_node() const{
-            return wrapped_node->get_node();
+        int32_t get_Id() override
+        {
+            return interface->get_Id();
+        }
+
+        void set_node(HabCom::Node* _node = nullptr) override
+        {
+            interface->set_Id(node.get());
+        }
+    
+        void sync_nodes() override {
+            interface->sync_nodes();
         }
 
     private:
-        std::shared_ptr<InternalBase> wrapped_node;
+        std::shared_ptr<InterfaceBase> interface;
+        std::shared_ptr<HabCom::Node> node;
     };
 
-
- //usage
-    class ImGuiNode : public NodeWrapper::InternalBase
+    // usage
+    class ImGuiNode : public InterfaceBase
     {
     private:
         std::unique_ptr<HabCom::Node> node;
